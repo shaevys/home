41c269e4b83c4540f9c39d5c8eca26c3b3fef79b
shaevys <86610416+shaevys@users.noreply.github.com>
2023-04-18 15:37:36 +0200

    Add files via upload

---
+++ assets/scripts/archives.js
@@ -0,0 +1,44 @@
+function winLGtoggle(){
+    var x = document.getElementById("win-lg");
+    var y = document.getElementById("win-sv");
+    var z = document.getElementById("win-jdf");
+
+    y.style.display = "none";
+    z.style.display = "none";
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function winSVtoggle(){
+    var x = document.getElementById("win-lg");
+    var y = document.getElementById("win-sv");
+    var z = document.getElementById("win-jdf");
+
+    x.style.display = "none";
+    z.style.display = "none";
+
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+
+function winJDFtoggle(){
+    var x = document.getElementById("win-lg");
+    var y = document.getElementById("win-sv");
+    var z = document.getElementById("win-jdf");
+
+    x.style.display = "none";
+    y.style.display = "none";
+
+    if (z.style.display === "none") {
+        z.style.display = "block";
+    } else {
+        z.style.display = "none";
+    }
+}
